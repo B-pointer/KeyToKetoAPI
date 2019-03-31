87ed1c7954773efd35e931a28fd72917b998ba26
Andrew Merts <andrew@andrewmerts.com>
2019-03-31 09:31:22 -0500

    added food table

---
+++ migrations/20190331092211_create_food_table.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('food', function(table) {
+  	table.increments('fid').primary();
+  	table.integer('uid');//might be null, foreign key?
+  	table.string('name').notNullable();
+  	table.float('servingsize').notNullable();
+  	table.enu('servingtype', ['volume', 'mass']);
+  	table.float('calories').notNullable();
+  	table.float('carbohydrates').notNullable();
+  	table.float('proteins').notNullable();
+  	table.float('fats').notNullable();
+  	table.float('sugar').notNullable();//do we need this in addition to carbs?
+  	table.float('sodium').notNullable();
+  	table.timestamps();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('food');
+};

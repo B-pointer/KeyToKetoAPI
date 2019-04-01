fb9d2bf5d23299437f268ad198efa7eb043f74e8
Andrew Merts <andrew@andrewmerts.com>
2019-04-01 07:49:30 -0500

    whoops, forgot to commit this with the last stuff

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const config = require('./config/config.js');
 const auth = require('./controller/auth.js');
 const knex = require('./controller/db.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.post('/login', (req, res) => {
+	if (!req.body.username || !req.body.password) {
+		return res.json({
+			success: false,
+			message: 'Must submit username and password'
+		});
+	}
+	
+	knex('user').where({username: req.body.username}).select('uid', 'password')
 	var token = auth.signToken({uid:1234});
 	res.json({
 		success: true,
@@ -16,7 +28,7 @@ app.post('/login', (req, res) => {
 app.get('/profile', auth.checkToken, (req, res) => {
 	res.json({
 		success: true,
-		message: "Profile data here"
+		message: 'Profile data here'
 	});
 });
 

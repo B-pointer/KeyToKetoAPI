3d389b3154cf5f416fb7236d8a06f224bca940f3
Andrew Merts <andrew@andrewmerts.com>
2019-04-23 23:58:54 -0500

    Added support for automatically quitting on inactivity

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('systemd');
+require('autoquit');
 const config = require('./config/config.js');
 const knex = require('./controller/db.js')(config);
 const auth = require('./controller/auth.js')(config, knex);
@@ -10,4 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 require('./controller/routes.js')(config, knex, auth, app);
 
-app.listen(config.listen);
+const server = require('http').createServer(app);
+if (config.autoquit)
+	server.autoQuit({timeOut: config.autoquit});
+server.listen(config.listen);
